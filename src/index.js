94931250bd1f670ac69f732d176e4bf68f51ec29
pawik077 <pawik.r.s.077@gmail.com>
2021-10-04 18:59:05 +0200

    Added first game

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { render } from "react-dom";
+import ProgressBar from "@ramonak/react-progress-bar"
+import './index.css'
+
+const CIRCLE_SCORE = 1
+const CIRCLE_NUMBER = 15
+
+const Timer = ({ time, interval = 100, onEnd }) => {
+	const [internalTime, setInternalTime] = React.useState(time)
+	const timerRef = React.useRef(time)
+	const timeRef = React.useRef(time)
+	React.useEffect(() => {
+		if (internalTime === 0 && onEnd) onEnd()
+	}, [internalTime, onEnd])
+	React.useEffect(() => {
+		timerRef.current = setInterval(() => setInternalTime(timeRef.current -= interval), interval)
+		return () => clearInterval(timerRef.current)
+	}, [interval])
+	//return <span>{`Time: ${internalTime}`}</span>
+	return (
+		<div id='progressBar'>
+			<span id='timer'>{`Pozostały czas: ${(internalTime / 1000).toFixed(1)}s`}</span>
+			<ProgressBar completed={internalTime / time * 100} isLabelVisible={false} transitionDuration='0.1s' transitionTimingFunction='linear' />
+		</div>
+	)
+}
+
+const Circle = ({ tap, isActive }) =>
+	<button
+		className={`circle ${isActive ? 'activeCircle' : 'inactiveCircle'}`}
+		onClick={() => { if (isActive) tap(CIRCLE_SCORE) }}
+	/>
+
+const Circles = ({ tap, activeId }) =>
+	<>
+		{new Array(CIRCLE_NUMBER).fill().map((_, id) =>
+			<Circle key={id} tap={tap} isActive={id === activeId} />)}
+	</>
+
+const Score = ({ value }) => <div>{`Score: ${value}`}</div>
+
+const Game = () => {
+	const [playing, setPlaying] = React.useState(false)
+	const [finished, setFinished] = React.useState(false)
+	const [score, setScore] = React.useState(0)
+	const [activeCircleId, setActiveCircleId] = React.useState(0)
+	const [gameTime, setGameTime] = React.useState('')
+	const startGame = () => {
+		setFinished(false)
+		setPlaying(true)
+		setScore(0)
+		setActiveCircleId(Math.floor(Math.random() * CIRCLE_NUMBER))
+	}
+	const endGame = () => {
+		setPlaying(false)
+		setFinished(true)
+	}
+	const tap = (points) => {
+		setScore(score + points)
+		let rn
+		do rn = Math.floor(Math.random() * CIRCLE_NUMBER)
+		while (rn === activeCircleId)
+		setActiveCircleId(rn)
+	}
+	return (
+		<div className='game'>
+			{!playing && !finished && (
+				<div className='welcome'>
+					<h1>Polowanie na przedmioty</h1>
+					<h3>Zalogowany jako: </h3>
+					<h3>Najlepszy wynik: </h3>
+					Czas gry: <input value={gameTime} onChange={e => setGameTime(e.target.value)} />
+					<button id='startButton' onClick={ Number.isInteger(parseFloat(gameTime)) ? startGame : null }>Start</button>
+				</div>
+			)}
+			{playing && (
+				<>
+					<button onClick={endGame}>End</button>
+					<Timer time={parseFloat(gameTime) * 1000} onEnd={endGame} />
+					<Score value={score}/>
+					<Circles tap={tap} activeId={activeCircleId}/>
+				</>
+			)}
+			{finished && (
+				<div className='endGame'>
+					<Score value={score} />
+					<p>Średni czas uderzenia: {(parseFloat(gameTime) * 1000 / score).toFixed(2)}ms</p>
+					<button onClick={startGame}>Zagraj ponownie</button>
+				</div>
+			)}
+		</div>
+	)
+}
+
+render(<Game id='game'/>, document.getElementById('root'))
